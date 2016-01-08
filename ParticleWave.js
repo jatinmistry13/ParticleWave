bc27bf88b598dbe8ece6b1d42609f273f1436d8e
Jatin Mistry <jatin.mistry@packetzoom.com>
2016-01-08 00:13:26 -0500

    initial commit

---
+++ ParticleWave.js
@@ -0,0 +1,45 @@
+/**
+ * Create a Particles Emitter system on canvas through javascript.
+ * @author Jatin Mistry
+ */
+ 
+(function() {
+    var d,canvas;
+    d = document;
+    canvas = d.body.appendChild(d.createElement('canvas'));
+    
+    //var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+
+    var width = 0;
+    var height = 0;
+
+    // Render the canvas context
+    function render(ctx) {
+        ctx.save();
+        
+        // TODO
+
+        ctx.restore();
+    }
+
+    // Update Canvas
+    function update() {
+        
+        // TODO
+    }
+
+    // Take a screenshot
+    function Screenshot() {
+        window.open(canvas.toDataURL());
+    }
+    
+    // Perform animation
+    (function animate() {
+        // rAF.js
+        requestAnimationFrame(animate);
+        update();
+        render(ctx);
+    })();
+    
+})();
